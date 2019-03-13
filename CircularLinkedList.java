4f1c9e33e793ef7baea64fd7741272d795f66d2a
rashiccv <32409006+merashi@users.noreply.github.com>
2019-03-13 11:01:02 +0530

    create method :public void addNodeToHead(int data)

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -27,15 +27,34 @@ public class CircularLinkedList {
             this.next = next;
         }
     }
-    public void print(){
-Node temp = head;
-do{
-    System.out.println(" " + temp.data);
-    temp = temp.next;
-}
-while (temp != head);
+    public void print() {
+        if (size == 0) {
+            System.out.println("List is empty");
+        } else {
+            Node temp = head;
+            do {
+                System.out.println(" " + temp.data);
+                temp = temp.next;
+            }
+            while (temp != head);
             System.out.println("");
 
+        }
+    }
+    public void addNodeToHead(int data){
+        Node n = new Node(data);
+        if(size == 0){
+            head = n;
+            tail = n;
+            n.next = head;
+        }
+        else{
+            Node temp = head;
+            n.next = temp;
+            head = n;
+            tail.next = head;
+        }
+        size++;
     }
 
 }
