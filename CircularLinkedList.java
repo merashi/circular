7fe532951965d33690c6c58d618882884fce1f8b
rashiccv <32409006+merashi@users.noreply.github.com>
2019-03-13 10:22:48 +0530

    create constructor:(int data , Node next)

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -10,7 +10,10 @@ public class CircularLinkedList {
         public Node(int data) {
             this.data = data;
         }
-
+        public Node(int data ,Node next){
+            this.data = data;
+            this.next = next;
+        }
 
     }
 }
